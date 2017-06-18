60fd94e6c265f1d69708550f7f463889c003c407
Daniel Castro García <dandev237@gmail.com>
2017-06-18 17:12:37 +0200

    Minor change to Link.js and Signup.js

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,3 +1,10 @@
 /**
- * Created by dande on 18/06/2017.
+ * Created by Daniel on 18/06/2017.
  */
+import React from 'react';
+
+export default class Link extends React.Component{
+    render() {
+        return <p>Link component here</p>;
+    }
+}
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -1,3 +1,10 @@
 /**
- * Created by dande on 18/06/2017.
+ * Created by Daniel on 18/06/2017.
  */
+import React from 'react';
+
+export default class Signup extends React.Component {
+    render(){
+        return <p>Signup component here</p>;
+    }
+}
