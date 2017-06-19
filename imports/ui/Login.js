e321d7e4dc5ab2a22a83e10fa3add7e66d30c7ef
Daniel Castro García <dandev237@gmail.com>
2017-06-19 22:16:04 +0200

    Login form implemented

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -3,14 +3,39 @@
  */
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Meteor} from 'meteor/meteor';
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  //Component state
+            error: ''
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault(); //Prevent browser default action (full refresh)
+
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+
+        Meteor.loginWithPassword({email}, password, (error) => {
+            console.log('Login callback', error);
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1>Login to Short Lnk</h1>
+                <h1>Short Lnk</h1>
 
-                Login form here
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" ref="email" name="email" placeholder="e-mail"/>
+                    <input type="password" ref="password" name="password" placeholder="password"/>
+                    <button>Login</button>
+                </form>
 
                 <Link to="/signup">Create an account</Link>
             </div>
