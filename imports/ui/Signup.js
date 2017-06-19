ccff2c9171e62514482652a09e1a281a0ca5e48c
Daniel Castro García <dandev237@gmail.com>
2017-06-19 20:22:16 +0200

    Signup form implemented

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -5,12 +5,33 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 export default class Signup extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  //Component state
+            error: ''
+        };
+    }
+
+    onSubmit(e) {
+        e.preventDefault(); //Prevent browser default action (full refresh)
+
+        this.setState({
+           error: 'Something went wrong.'
+        });
+    }
+
     render(){
         return (
             <div>
                 <h1>Join Short Lnk</h1>
 
-                Signup form here
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" name="email" placeholder="e-mail"/>
+                    <input type="password" name="password" placeholder="password"/>
+                    <button>Create Account</button>
+                </form>
 
                 <Link to="/">Already have an account?</Link>
             </div>
