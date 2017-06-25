d9f8b2617477631d88ecd07c47018e661c180c9d
Daniel Castro García <dandev237@gmail.com>
2017-06-25 16:45:03 +0200

    Middleware redirection by link ID implemented

--- server/main.js
+++ server/main.js
@@ -1,8 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';  //This will execute the code inside users.js
-import '../imports/api/links';
+import { Links } from '../imports/api/links';
 
+//Middleware redirection
 Meteor.startup(() => {
+    WebApp.connectHandlers.use((req, res, next) => {
+        const _id = req.url.slice(1);
+        const link = Links.findOne({_id});   //ids are unique
 
+        if(link) {
+            res.statusCode = 302;
+            res.setHeader('Location', link.url);
+            res.end();
+        } else {
+            next();
+        }
+    });
 });
