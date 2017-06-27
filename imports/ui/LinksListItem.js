8b129aef44d56eef57001ea0acaed8f96986ddc6
Daniel Castro García <dandev237@gmail.com>
2017-06-27 21:49:26 +0200

    clipboard npm package added, 'Copy' button added to LinksListItem

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -3,10 +3,25 @@
  */
 import React from 'react';
 import PropTypes from 'proptypes';
+import Clipboard from 'clipboard';
 
 export default class LinksListItem extends React.Component {
     constructor(props){
         super(props);
+    }
+
+    componentDidMount(){
+        this.clipboard = new Clipboard(this.refs.copyButton);
+
+        this.clipboard.on('success', () => {
+            //TODO Give success feedback
+        }).on('error', () => {
+            //TODO Give error feedback
+        });
+    }
+
+    componentWillMount(){
+        this.clipboard.destroy();
     }
 
     render() {
@@ -14,6 +29,7 @@ export default class LinksListItem extends React.Component {
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
+                <button ref="copyButton" data-clipboard-text={this.props.shortUrl}>Copy</button>
             </div>
         );
     }
