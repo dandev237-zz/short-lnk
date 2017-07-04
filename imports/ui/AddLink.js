7ef2b4f853c76be43869b0ba20fb4d41bdb76ef4
Daniel Castro García <dandev237@gmail.com>
2017-07-04 19:49:16 +0200

    Link filtering refactored (Controlled input)

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -4,15 +4,33 @@
 import React from 'react';
 
 export default class AddLink extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: ''
+        };
+    }
+
     onSubmit(e) {
-        const url = this.refs.url.value.trim();
+        const url = this.state.url;
 
         e.preventDefault();
 
         if(url) {
-            Meteor.call('links.insert', url);
-            this.refs.url.value = '';
+            Meteor.call('links.insert', url, (err, res) => {
+                if(!err){
+                    this.setState({
+                        url: ''
+                    });
+                }
+            });
         }
+    }
+
+    onChange(e) {
+        this.setState({
+            url: e.target.value
+        });
     }
 
     render(){
@@ -20,7 +38,7 @@ export default class AddLink extends React.Component {
             <div>
                 <p>Add Link</p>
                 <form onSubmit={this.onSubmit.bind(this)}>
-                    <input type="text" ref="url" placeholder="URL"/>
+                    <input type="text" placeholder="URL" value={this.state.url} onChange={this.onChange.bind(this)}/>
                     <button>Add Link</button>
                 </form>
             </div>
