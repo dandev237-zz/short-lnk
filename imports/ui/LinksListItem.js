d85a1fa934daa4a601f6a371d65e68f6617fcbe0
Daniel Castro García <dandev237@gmail.com>
2017-07-04 20:37:44 +0200

    moment.js installed to manage last visit dates

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -5,6 +5,7 @@ import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import PropTypes from 'proptypes';
 import Clipboard from 'clipboard';
+import moment from 'moment';
 
 export default class LinksListItem extends React.Component {
     constructor(props){
@@ -31,13 +32,24 @@ export default class LinksListItem extends React.Component {
         this.clipboard.destroy();
     }
 
+    renderStats() {
+        const visitMessage = this.props.visitedCount === 1 ? 'visit' : 'visits';
+        let visitedMessage = null;
+
+        if(typeof this.props.lastVisitedAt === 'number') {
+            visitedMessage = `(visited ${moment(this.props.lastVisitedAt).fromNow()})`;
+        }
+
+        return <p>{this.props.visitedCount} {visitMessage} {visitedMessage}</p>;
+    }
+
     render() {
         return(
             <div>
                 <p>{this.props.url}</p>
                 <p>{this.props.shortUrl}</p>
                 <p>{this.props.visible.toString()}</p>
-                <p>{this.props.visitedCount} - {this.props.lastVisitedAt}</p>
+                {this.renderStats()}
                 <button ref="copyButton" data-clipboard-text={this.props.shortUrl}>
                     {this.state.justCopied ? 'Copied' : 'Copy'}
                 </button>
