09d82787b45b283ed003b141cda3a9e303e42e27
Daniel Castro García <dandev237@gmail.com>
2017-07-09 18:17:47 +0200

    Header styles added

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import Modal from 'react-modal';
+import {Meteor} from 'meteor/meteor';
 
 export default class AddLink extends React.Component {
     constructor(props) {
@@ -49,17 +50,30 @@ export default class AddLink extends React.Component {
                     isOpen={this.state.isOpen}
                     contentLabel="Add link"
                     onAfterOpen={() => this.refs.url.focus()}
-                    onRequestClose={this.handleModalClose.bind(this)}>
+                    onRequestClose={this.handleModalClose.bind(this)}
+                    // className="boxed-view__box"
+                    // overlayClassName="boxed-view boxed-view--modal">
+                    className={{
+                        base: 'boxed-view__box',
+                        afterOpen: 'boxed-view__box',
+                        beforeClose: 'boxed-view__box'
+                    }}
+                    overlayClassName={{
+                        base: 'boxed-view boxed-view--modal',
+                        afterOpen: 'boxed-view boxed-view--modal',
+                        beforeClose: 'boxed-view boxed-view--modal'
+                    }}>
                     <h1>Add Link</h1>
                     {this.state.error ? <p>{this.state.error}</p> : undefined}
-                    <form onSubmit={this.onSubmit.bind(this)}>
+                    <form className="boxed-view__form" onSubmit={this.onSubmit.bind(this)}>
                         <input type="text" placeholder="URL" value={this.state.url} onChange={this.onChange.bind(this)}
                             ref="url"/>
-                        <button>Add Link</button>
+                        <button className="button">Add Link</button>
+                        <button type="button" className="button button--secondary" onClick={this.handleModalClose.bind(this)}>
+                            Cancel
+                        </button>
                     </form>
-                    <button onClick={this.handleModalClose.bind(this)}>
-                        Cancel
-                    </button>
+
                 </Modal>
             </div>
         );
