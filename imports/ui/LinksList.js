ea900506c0ae8270d601ac082abc91ac9fb8ae85
Daniel Castro García <dandev237@gmail.com>
2017-07-09 18:50:14 +0200

    react-flip-move added, styles finished

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -5,9 +5,11 @@ import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import {Tracker} from 'meteor/tracker';
 import {Session} from 'meteor/session';
+import FlipMove from 'react-flip-move';
 
 import {Links} from '../api/links';
 import LinksListItem from './LinksListItem';
+
 
 export default class LinksList extends React.Component {
     constructor(props) {
@@ -36,19 +38,26 @@ export default class LinksList extends React.Component {
     }
 
     renderLinksListItems() {
-        return this.state.links.map((link) => {
-            const shortUrl = Meteor.absoluteUrl(link._id);
-            return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>;
-        });
+        if(this.state.links.length === 0) {
+            return(
+                <div className="item">
+                    <p className="item__status-message">No Links Found</p>
+                </div>
+            );
+        } else {
+            return this.state.links.map((link) => {
+                const shortUrl = Meteor.absoluteUrl(link._id);
+                return <LinksListItem key={link._id} shortUrl={shortUrl} {...link}/>;
+            });
+        }
     }
 
     render() {
         return (
             <div>
-                <p>Links List</p>
-                <div>
+                <FlipMove maintainContainerHeight={true}>
                     {this.renderLinksListItems()}
-                </div>
+                </FlipMove>
             </div>
         );
     }
