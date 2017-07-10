86b0f24a128b1c5fbfdedb8368937573694e3cf4
Daniel Castro García <dandev237@gmail.com>
2017-07-10 20:06:47 +0200

    Modal style error corrected

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -41,39 +41,28 @@ export default class AddLink extends React.Component {
     }
 
     render(){
-        return(
+        return (
             <div>
-                <button className="button" onClick={() => this.setState({isOpen: true})}>
-                    + Add Link
-                </button>
+                <button className="button" onClick={() => this.setState({isOpen: true})}>+ Add Link</button>
                 <Modal
                     isOpen={this.state.isOpen}
                     contentLabel="Add link"
                     onAfterOpen={() => this.refs.url.focus()}
                     onRequestClose={this.handleModalClose.bind(this)}
-                    // className="boxed-view__box"
-                    // overlayClassName="boxed-view boxed-view--modal">
-                    className={{
-                        base: 'boxed-view__box',
-                        afterOpen: 'boxed-view__box',
-                        beforeClose: 'boxed-view__box'
-                    }}
-                    overlayClassName={{
-                        base: 'boxed-view boxed-view--modal',
-                        afterOpen: 'boxed-view boxed-view--modal',
-                        beforeClose: 'boxed-view boxed-view--modal'
-                    }}>
+                    className="boxed-view__box"
+                    overlayClassName="boxed-view boxed-view--modal">
                     <h1>Add Link</h1>
                     {this.state.error ? <p>{this.state.error}</p> : undefined}
-                    <form className="boxed-view__form" onSubmit={this.onSubmit.bind(this)}>
-                        <input type="text" placeholder="URL" value={this.state.url} onChange={this.onChange.bind(this)}
-                            ref="url"/>
+                    <form onSubmit={this.onSubmit.bind(this)} className="boxed-view__form">
+                        <input
+                            type="text"
+                            placeholder="URL"
+                            ref="url"
+                            value={this.state.url}
+                            onChange={this.onChange.bind(this)}/>
                         <button className="button">Add Link</button>
-                        <button type="button" className="button button--secondary" onClick={this.handleModalClose.bind(this)}>
-                            Cancel
-                        </button>
+                        <button type="button" className="button button--secondary" onClick={this.handleModalClose.bind(this)}>Cancel</button>
                     </form>
-
                 </Modal>
             </div>
         );
